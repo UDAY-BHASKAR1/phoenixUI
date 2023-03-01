d9cbb036c8ed61a614501bbb2cd544667a271c36
UDAY-BHASKAR1 <udaybhaskarambati@gmail.com>
2023-03-01 10:34:49 +0530

    first commit

---
+++ primeng/primeng.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ButtonModule} from 'primeng/button';
+
+
+
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    CommonModule,
+    ButtonModule
+  ]
+})
+export class PrimengModule { }
